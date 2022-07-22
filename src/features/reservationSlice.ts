ab85de1d78c80e1818f99497a28772266bfcdc5f
Christian AS <alvitesscf18@uww.edu>
2022-07-22 00:54:19 -0500

    finished app

---
+++ src/features/reservationSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+interface ReservationState {
+	value: string[]
+}
+
+const initialState: ReservationState = {
+	value: ['Selena G'],
+}
+
+export const reservationsSlice = createSlice({
+	name: 'reservations',
+	initialState,
+	reducers: {
+		addReservation: (
+			state: ReservationState,
+			action: PayloadAction<string>,
+		) => {
+			state.value.push(action.payload)
+		},
+		removeReservation: (
+			state: ReservationState,
+			action: PayloadAction<number>,
+		) => {
+			state.value.splice(action.payload, 1)
+		},
+	},
+})
+
+export const { addReservation, removeReservation } = reservationsSlice.actions
+export default reservationsSlice.reducer
